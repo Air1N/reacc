c9de787eb6ee0a6bf460283fdbcd014d3d637a5f
UnknownEvil <reapor12@hotmail.com>
2017-09-20 18:56:56 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 80;
+var wowclicks = 0;
+var angryclicks = 0;
+
+var allClients = -1;
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/lib', express.static(__dirname + '/lib'));
+app.use('/', express.static(__dirname + '/'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    allClients++;
+    var UserID = allClients;
+    
+    console.log('ID: ' + UserID + ' connected.');
+    io.emit('userConnect', {
+        UserID: UserID
+    });
+    
+    socket.on('angryclick', function () {
+      socket.broadcast.emit('angryclick');
+    });
+    
+    socket.on('wowclick', function () {
+      socket.broadcast.emit('wowclick');
+    });
+    
+    socket.once('disconnect', function() {
+        console.log('ID: ' + UserID + ' disconnected.');
+        
+        socket.broadcast.emit('userDisconnect', {
+            UserID: UserID
+        });
+    });
+});
+
+http.listen(port, function() {
+    console.log('listening on *:' + (port).toString());
+});
